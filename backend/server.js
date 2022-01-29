40f8929715fbe332eb2e0ac01fafacd3e0d39a10
ruslanhaziahmetov <ruslan.xaziaxmetov@mail.ru>
2022-01-29 21:19:29 +0500

    add static

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 const dashboard = require('./router/dashboardRouter')
 
+
+app.use(express.static(path.resolve(__dirname,'../build')))
 app.use(express.json())
 app.use(cors())
 
 app.use('/dashboard',dashboard)
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../build','index.html'))
+})
 
 
 app.listen(process.env.PORT,()=>{
